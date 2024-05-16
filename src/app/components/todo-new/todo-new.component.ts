0f86ee72b1018cc397d942dca0494c47f366a1f2
Fran <franamezcuamedina@gmail.com>
2024-05-16 23:41:20 +0200

    validations

--- src/app/components/todo-new/todo-new.component.ts
+++ src/app/components/todo-new/todo-new.component.ts
@@ -29,12 +29,15 @@ export class TodoNewComponent implements OnInit {
 
   onSubmit() {
 
-    if (this.task.title == '' || this.task.description == '' || this.task.priority == '' || this.task.status == '') return
+    if (this.task.title == '' || this.task.description == '' || this.task.priority == '' || this.task.status == '') {
+      alert("Todos los campos deben estar rellenos")
+      return
+    }
 
     const newTodo = new TaskModel(uuidv4(), this.task.title, this.task.description, this.task.status, this.task.priority);
 
     this.taskService.addTask(newTodo)
-      .subscribe()
+      .subscribe(()=>{alert("Tarea creada con exito")})
 
   }
 
